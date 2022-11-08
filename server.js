b7636738ae20f411994cde7055cdbb70eaedc333
KevMaillard <kemaillard@gmail.com>
2022-11-08 18:42:47 +0100

    test ports

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ io.on('connection', socket => {
 
 
 
-server.listen(3000, () => {
-    console.log("Server running port 3000");
+server.listen(443, () => {
+    console.log("Server running port 443");
 });
